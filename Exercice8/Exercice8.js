3caac64e0c551b595104a9f87f4dfe6a1f34ffab
RyzaaJL <johanladent@gmail.com>
2019-01-08 16:59:30 +0100

    javascript

---
+++ Exercice8/Exercice8.js
@@ -0,0 +1,8 @@
+
+  var age = parseInt(document.getElementById('age'));
+  if (age < 18) {
+      alert("Vous êtes mineur(e)");
+    }
+  else (age >= 18) {
+      alert("Vous êtes majeur(e)")
+    }
